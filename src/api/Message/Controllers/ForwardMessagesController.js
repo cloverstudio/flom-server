43301452d2dbca437c2681cad80e7b3a4d9b4cc5
petarb1988 <petar.biocic@gmail.com>
2026-03-18 12:49:06 +0100

    possible fix for forward messages bug

--- src/api/Message/Controllers/ForwardMessagesController.js
+++ src/api/Message/Controllers/ForwardMessagesController.js
@@ -21,8 +21,10 @@ router.post("/", auth({ allowUser: true }), async function (request, response) {
       message.userID = userId;
 
       const messageObj = await sendMessage(message);
-      console.log("ForwardMessagesController: messageObj", JSON.stringify(messageObj, null, 2));
-      messageList.push({ _id: messageObj._id.toString(), created: messageObj.created });
+
+      if (messageObj && messageObj._id) {
+        messageList.push({ _id: messageObj._id.toString(), created: messageObj.created });
+      }
     }
 
     return Base.successResponse(response, Const.responsecodeSucceed, { messages: messageList });
