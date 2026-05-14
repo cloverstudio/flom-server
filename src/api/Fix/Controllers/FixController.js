0e1261436f505df721d8a4946d5f900bd34ba2c8
petarb1988 <petar.biocic@gmail.com>
2026-05-14 11:31:14 +0200

    test #2

--- src/api/Fix/Controllers/FixController.js
+++ src/api/Fix/Controllers/FixController.js
@@ -445,16 +445,18 @@ router.get("/wa", async (request, response) => {
     // Like the browser fetch API, the default method is GET
     const r = await fetch(whatsAppPricingUrl, {
       headers: {
-        Authority: "developers.facebook.com",
-        "User-Agent":
-          "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/147.0.0.0 Safari/537.36",
+        "User-Agent": "Mozilla/5.0",
         Accept:
           "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
+        "Accept-Language": "en-US,en;q=0.9",
       },
     });
 
-    console.log("STATUS:", r.status);
-    console.log("FINAL URL:", r.url);
+    console.log({
+      status: r.status,
+      redirected: r.redirected,
+      finalUrl: r.url,
+    });
 
     const data = await r.text();
 
