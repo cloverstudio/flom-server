d5fa183521732d9c508af329620acb695634a255
petarb1988 <petar.biocic@gmail.com>
2026-05-14 11:26:27 +0200

    attempt to fix wa prices scraper #2

--- src/api/Fix/Controllers/FixController.js
+++ src/api/Fix/Controllers/FixController.js
@@ -445,9 +445,11 @@ router.get("/wa", async (request, response) => {
     // Like the browser fetch API, the default method is GET
     const r = await fetch(whatsAppPricingUrl, {
       headers: {
+        Authority: "developers.facebook.com",
         "User-Agent":
-          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/122.0.0.0 Safari/537.36",
-        Accept: "text/html,application/xhtml+xml",
+          "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/147.0.0.0 Safari/537.36",
+        Accept:
+          "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
       },
     });
     const data = await r.text();
