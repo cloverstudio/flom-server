580526835a549a33d7ebb6c1119299526af32f8f
petarb1988 <petar.biocic@gmail.com>
2026-05-18 15:41:04 +0200

    quick fix

--- src/api/WhatsApp/routes.js
+++ src/api/WhatsApp/routes.js
@@ -3,6 +3,6 @@ const router = require("express").Router();
 router.use("/whatsapp/send", require("./Controllers/SendWhatsAppMessageController"));
 router.use("/whatsapp/cb", require("./Controllers/WhatsAppCallbackController"));
 router.use("/whatsapp/phone-number", require("./Controllers/GetWhatsAppPhoneNumber"));
-router.use("/whatsapp/chat-status", require("./Controllers/GetWhatsAppChatStatus"));
+router.use("/whatsapp", require("./Controllers/GetWhatsAppChatStatus"));
 
 module.exports = router;
