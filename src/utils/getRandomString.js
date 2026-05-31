37a8fc79584748fbd6aa96ab25932932b3486540
petarb1988 <petar.biocic@gmail.com>
2026-05-31 10:46:47 +0200

    implemented crypto in get random string

--- src/utils/getRandomString.js
+++ src/utils/getRandomString.js
@@ -1,19 +1,22 @@
+const crypto = require("crypto");
+
 const possible = {
   alphanumeric: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789",
   numeric: "0123456789",
   alpha: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz",
 };
 
-function getRandomString(length, type) {
+function getRandomString(length, type = "alphanumeric") {
   let text = "";
-  const possibleChars = possible[type] || possible.alphanumeric;
+  const possibleChars = possible[type];
 
   if (!length) {
     length = 32;
   }
 
-  for (let i = 0; i < length; i++)
-    text += possibleChars.charAt(Math.floor(Math.random() * possibleChars.length));
+  const rand = crypto.randomInt(0, possibleChars.length);
+
+  for (let i = 0; i < length; i++) text += possibleChars.charAt(rand);
 
   return text;
 }
