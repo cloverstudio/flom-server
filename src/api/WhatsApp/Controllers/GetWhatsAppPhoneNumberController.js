fce641d4bc3993bfc5900e2ec9751aeace543a35
petarb1988 <petar.biocic@gmail.com>
2026-06-01 22:51:15 +0200

    get wa phone number api extended

--- src/api/WhatsApp/Controllers/GetWhatsAppPhoneNumberController.js
+++ src/api/WhatsApp/Controllers/GetWhatsAppPhoneNumberController.js
@@ -5,9 +5,10 @@
  * @apiVersion 2.0.34
  * @apiName Get WhatsApp phone number
  * @apiGroup WebAPI WhatsApp
- * @apiDescription Returns the WhatsApp phone number.
+ * @apiDescription Returns the WhatsApp phone number. If userPhoneNumber and businessPhoneNumber parameters are provided, it creates a link between the user's business number and his account phone number. If only userPhoneNumber is provided, it is presumed that the user's business number and account phone number are the same, and the link is created accordingly.
  *
- * @apiHeader {String} access-token Users unique access-token.
+ * @apiParam {Query string}  [userPhoneNumber]      User's phone number
+ * @apiParam {Query string}  [businessPhoneNumber]  User's business phone number (if user's number and user's business number are the same, no need to send)
  *
  * @apiSuccessExample Success Response
  * {
@@ -30,9 +31,32 @@
 const router = require("express").Router();
 const Base = require("../../Base");
 const { Const, Config } = require("#config");
+const Utils = require("#utils");
+const { User } = require("#models");
 
 router.get("/", async function (request, response) {
   try {
+    let { userPhoneNumber, businessPhoneNumber } = request.query;
+
+    if (userPhoneNumber) {
+      userPhoneNumber = Utils.formatPhoneNumber(userPhoneNumber);
+    }
+
+    if (businessPhoneNumber) {
+      businessPhoneNumber = Utils.formatPhoneNumber(businessPhoneNumber);
+    }
+
+    if (userPhoneNumber && !businessPhoneNumber) {
+      businessPhoneNumber = userPhoneNumber;
+    }
+
+    if (businessPhoneNumber) {
+      await User.updateOne(
+        { phoneNumber: userPhoneNumber },
+        { "whatsApp.businessPhoneNumber": businessPhoneNumber },
+      );
+    }
+
     return Base.successResponse(response, Const.responsecodeSucceed, {
       phoneNumber: Config.whatsAppPhoneNumber || "",
     });
