a214baa8d6cbc9b614a3117fa57131e87f2ce521
petarb1988 <petar.biocic@gmail.com>
2026-06-02 17:22:42 +0200

    small fix

--- src/api/WhatsApp/helpers/getMessageTypeAndAsset.js
+++ src/api/WhatsApp/helpers/getMessageTypeAndAsset.js
@@ -1,4 +1,4 @@
-const { logger } = require("#utils");
+const { logger } = require("#infra");
 const { Const, Config } = require("#config");
 const { FlomFile } = require("#models");
 const Utils = require("#utils");
