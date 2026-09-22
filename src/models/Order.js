451ccc971e31082d8a8358d458152dae9487f63d
petarb1988 <petar.biocic@gmail.com>
2026-09-22 14:50:37 +0200

    changed business in order model

--- src/models/Order.js
+++ src/models/Order.js
@@ -7,7 +7,7 @@ const mongoose = require("mongoose");
 
 const schema = new mongoose.Schema(
   {
-    businessId: String,
+    business: { _id: String, name: String },
     price: { countryCode: String, currency: String, value: Number, valueInSats: Number },
     products: [{ _id: String, name: String, condition: String, quantity: Number, file: {} }],
     seller: {
